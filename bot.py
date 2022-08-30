737a3538fb27291883ff304b03d0f8d6c51ad3eb
rictigr02 <105606006+rictigr02@users.noreply.github.com>
2022-08-30 17:58:12 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ import json
 import imdb
 import os
 PORT = int(os.environ.get('PORT', 5000))
-api_key= #Enter your Telegram bot token
+api_key= 5774122252:AAHM-87qRthbgj1NbvC6zGNvsmSokVzgOXw
 ia = imdb.IMDb() 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
